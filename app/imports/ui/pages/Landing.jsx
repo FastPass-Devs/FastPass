7538cf7fb8040c94fce3d3b815f3ce3159f1fce3
AnatAmar1 <aa91@hawaii.edu>
2022-02-06 19:30:22 -1000

    Landing Page In progress

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Button } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
+      <div className= "landing">
+        <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
 
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
+          <Grid.Column width={4}>
+            <h1 className="ui red header">Fast</h1>
+            <h1 className="ui red header">Pass</h1>
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <h5 className="ui red header">Sign In</h5>
+            <input placeholder= "Email"/>
+            <input placeholder= "Password"/>
+            <Button type='submit'>Submit</Button>
+            <h5 className="ui red header">Dont have an account? Sign Up</h5>
+          </Grid.Column>
+        </Grid>
+      </div>
     );
   }
 }
