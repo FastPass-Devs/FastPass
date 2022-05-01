9a9ab9c3d2bd73c97157846c8ea9d2a641951450
Joshua Paino <62793439+joshipaino@users.noreply.github.com>
2022-04-30 21:51:33 -1000

    QR code now shows up

--- app/imports/ui/pages/MultiFactorAuthenticationSettings.jsx
+++ app/imports/ui/pages/MultiFactorAuthenticationSettings.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Button, Header, Image } from 'semantic-ui-react';
+import { Container, Button, Header, Image, Segment, Divider } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Buffer } from 'buffer';
@@ -11,13 +11,12 @@ class MultiFactorAuthenticationSettings extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { show2fa: true };
-    this.qrCode = null;
+    this.state = { show2fa: true, qrCode: null };
   }
 
   activate2fa = () => {
     Accounts.generate2faActivationQrCode(
-      'meteor-application-template-react',
+      'UH-FastPass',
       (err, result) => {
         if (err) {
           console.error('Error retrieving QR code', err);
@@ -25,8 +24,9 @@ class MultiFactorAuthenticationSettings extends React.Component {
         }
         // eslint-disable-next-line no-unused-vars
         const { svg, secret, uri } = result;
-        console.log(Buffer.from(svg).toString('base64'));
-        this.setState({ qrCode: Buffer.from(svg).toString('base64') });
+        const something = Buffer.from(svg).toString('base64');
+        console.log(something);
+        this.setState({ qrCode: something });
       },
     );
   }
@@ -36,9 +36,15 @@ class MultiFactorAuthenticationSettings extends React.Component {
     return (
       <Container>
         <Header as="h1">Multi-factor Authentication Settings</Header>
-        <Button onClick={this.activate2fa}> Generate QR code</Button>
-        {console.log(this.qrCode)}
-        <Image width="20" src={`data:images/svg+xml;base64,${this.qrCode}`}/>
+        <Segment>
+          <Header as="h2">Want to enable 2FA? Follow these steps:</Header>
+          <Header as="h3">1. Get a QR code by clicking the button below and scan it in <b>Google Authenticator</b></Header>
+          <Button onClick={this.activate2fa}> Generate QR code</Button>
+          {(this.state.qrCode === null) ? '' :
+            <Image size="medium" width="20" src={`data:image/svg+xml;base64,${this.state.qrCode}`}/>}
+          <Divider/>
+          <Header as="h3">2. Input your code here:</Header>
+        </Segment>
       </Container>
     );
   }
