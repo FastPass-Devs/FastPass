85f81d3fab06f1742bf67c11733c5006cf6ba88a
justin-mar <70727597+justin-mar@users.noreply.github.com>
2022-05-01 21:00:59 -1000

    Added images and cleaned up for it

--- app/imports/ui/pages/EditStuff.jsx
+++ app/imports/ui/pages/EditStuff.jsx
@@ -31,7 +31,7 @@ class EditStuff extends React.Component {
     return (
       <Grid container centered>
         <Grid.Column>
-          <Header as="h2" textAlign="center">Edit Password</Header>
+          <Header as="h2" textAlign="center">Edit Login</Header>
           <AutoForm schema={bridge} onSubmit={data => this.submit(data)} model={this.props.doc}>
             <Segment>
               <TextField name='site'/>
